b22b67048fa61ca52123c016bf1c2d7cd737f270
Christian <christiantenz7@gmail.com>
2024-05-05 19:18:15 -0500

    pagina inicial de devjobs terminada, falta cambiar textos

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,21 +1,247 @@
-import React from 'react';
+import React from "react";
 
 export const Info = () => {
   return (
-    <div className="max-w-3xl mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-4 text-[#a98467] text-center border-b-2 border-[#6c584c]">Encuentra tu próximo empleo con DevJobs</h1>
+    <>
+      <section className="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat">
+        <div className="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        <img className="rounded-lg shadow-md hover:scale-105 transition-transform duration-300" src="/1.jpg" alt="Imagen 1" />
-        <img className="rounded-lg shadow-md hover:scale-105 transition-transform duration-300" src="/2.jpg" alt="Imagen 2" />
-        <img className="rounded-lg shadow-md hover:scale-105 transition-transform duration-300" src="/3.jpg" alt="Imagen 3" />
-        <img className="rounded-lg shadow-md hover:scale-105 transition-transform duration-300" src="/4.jpg" alt="Imagen 4" />
-      </div>
+        <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
+          <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+            <h1 className="text-3xl font-extrabold sm:text-5xl">
+              Bienvenido a DevJobs
+              <strong className="block font-extrabold text-rose-700">
+                {" "}
+                Forever Home.{" "}
+              </strong>
+            </h1>
 
-      <p className="text-lg mt-8 mb-4">Bienvenido a DevJobs, el mejor lugar para buscar empleo en la industria tecnológica. Ya sea que estés buscando tu primer trabajo en el sector o que quieras dar el próximo paso en tu carrera, estamos aquí para ayudarte a encontrar las mejores oportunidades laborales.</p>
-      <p className="text-lg mb-4">En DevJobs, ofrecemos una amplia variedad de ofertas laborales en áreas como desarrollo web, inteligencia artificial, ciencia de datos, ciberseguridad y mucho más. Nuestro objetivo es conectar a profesionales talentosos con empresas innovadoras que buscan su experiencia y habilidades.</p>
-      <p className="text-lg mb-4">Con nuestra plataforma fácil de usar, puedes explorar las últimas ofertas de empleo, crear un perfil profesional destacado, y postularte a las oportunidades que más te interesen. ¡No esperes más para dar el siguiente paso en tu carrera!</p>
-      <p className="text-lg mb-4">Regístrate ahora en DevJobs y empieza a construir tu futuro profesional hoy mismo. ¡El trabajo de tus sueños te está esperando!</p>
-    </div>
+            <p className="mt-4 max-w-lg sm:text-xl/relaxed font-bold">
+              El mejor lugar para buscar empleo en la industria tecnológica.
+            </p>
+
+            <div className="mt-8 flex flex-wrap gap-4 text-center">
+              <a
+                href="#"
+                className="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
+              >
+                Get Started
+              </a>
+
+              <a
+                href="#"
+                className="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-rose-600 shadow hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto"
+              >
+                Learn More
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="bg-gray-900 text-white">
+        <div className="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+          <div className="max-w-xl">
+            <h2 className="text-3xl font-bold sm:text-4xl">
+              What makes us <span className="text-rose-600">special</span>
+            </h2>
+
+            <p className="mt-4 text-gray-300">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+              dolores iure fugit totam iste obcaecati. Consequatur ipsa quod
+              ipsum sequi culpa delectus, cumque id tenetur quibusdam, quos fuga
+              minima.
+            </p>
+          </div>
+
+          <div className="mt-8 grid grid-cols-1 gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-3">
+            <div className="flex items-start gap-4">
+              <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+                <svg
+                  className="h-5 w-5"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                  ></path>
+                </svg>
+              </span>
+
+              <div>
+                <h2 className="text-lg font-bold">Lorem, ipsum dolor.</h2>
+
+                <p className="mt-1 text-sm text-gray-300">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                  cumque tempore est ab possimus quisquam reiciendis tempora
+                  animi! Quaerat, saepe?
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-start gap-4">
+              <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+                <svg
+                  className="h-5 w-5"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                  ></path>
+                </svg>
+              </span>
+
+              <div>
+                <h2 className="text-lg font-bold">Lorem, ipsum dolor.</h2>
+
+                <p className="mt-1 text-sm text-gray-300">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                  cumque tempore est ab possimus quisquam reiciendis tempora
+                  animi! Quaerat, saepe?
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-start gap-4">
+              <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+                <svg
+                  className="h-5 w-5"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                  ></path>
+                </svg>
+              </span>
+
+              <div>
+                <h2 className="text-lg font-bold">Lorem, ipsum dolor.</h2>
+
+                <p className="mt-1 text-sm text-gray-300">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                  cumque tempore est ab possimus quisquam reiciendis tempora
+                  animi! Quaerat, saepe?
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-start gap-4">
+              <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+                <svg
+                  className="h-5 w-5"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                  ></path>
+                </svg>
+              </span>
+
+              <div>
+                <h2 className="text-lg font-bold">Lorem, ipsum dolor.</h2>
+
+                <p className="mt-1 text-sm text-gray-300">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                  cumque tempore est ab possimus quisquam reiciendis tempora
+                  animi! Quaerat, saepe?
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-start gap-4">
+              <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+                <svg
+                  className="h-5 w-5"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                  ></path>
+                </svg>
+              </span>
+
+              <div>
+                <h2 className="text-lg font-bold">Lorem, ipsum dolor.</h2>
+
+                <p className="mt-1 text-sm text-gray-300">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                  cumque tempore est ab possimus quisquam reiciendis tempora
+                  animi! Quaerat, saepe?
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-start gap-4">
+              <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+                <svg
+                  className="h-5 w-5"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                  ></path>
+                </svg>
+              </span>
+
+              <div>
+                <h2 className="text-lg font-bold">Lorem, ipsum dolor.</h2>
+
+                <p className="mt-1 text-sm text-gray-300">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+                  cumque tempore est ab possimus quisquam reiciendis tempora
+                  animi! Quaerat, saepe?
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
--- src/views/Principal.jsx
+++ src/views/Principal.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Header } from '../components/Header'
 import { Info } from '../components/Info'
-import { Outlet } from 'react-router-dom'
+import { Footer } from '../components/Footer'
 
 export const Principal = () => {
   return (
     <>
         <Header/>
         <Info/>
+        <Footer/>
     </>
   )
 }
